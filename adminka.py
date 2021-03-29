8bbde03e2e78d1cc5145ac1cfa10a325e3f1c183
lichvsgm@gmail.com <vovaslobodan4@gmail.com>
2021-03-29 09:15:11 +0300

    fixed adminka.py

--- adminka.py
+++ adminka.py
@@ -1,9 +1,18 @@
 from config import *
+bot = telebot.TeleBot(TOKEN)
 
-def send(lesson):
-    markup = types.InlineKeyboardMarkup()
-    btn = types.InlineKeyboardButton(text = 'Увійти', url = lesson[2])
-    markup.add(btn)
-    bot.send_message(chat_id = idi, text = lesson[0], reply_markup = markup)
-    t.sleep(300)
-    bot.send_message(chat_id = idi, text = lesson[1], reply_markup = markup)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	print(message.chat.id)
+	if message.chat.id == admin_m:
+		bot.send_message(admin_m, 'Привет, админ!')
+	elif message.chat.id == admin_v:
+		bot.send_message(admin_v, 'Привет, админ!')
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+	if message.text.lower() == 'отправить' and message.chat.id == admin_v or message.chat.id == admin_m:
+		bot.send_message(message.chat.id, 'Привет, мой создатель')
+	elif message.text.lower() == 'изменить' and message.chat.id == admin_v or message.chat.id == admin_m:
+		bot.send_message(message.chat.id, 'Прощай, создатель')
+bot.polling()
